6140586e2dde4f4a41c0d9aba484754c10682ea1
covenantebube <covenantebube1@gmail.com>
2023-04-03 19:30:05 -0700

    load and hash(pathname) event

--- src/js/controller.js
+++ src/js/controller.js
@@ -35,13 +35,18 @@ const renderSpinner = function(parentEl){
 
 const showRecipe = async function (){
   try {
+    // const id = window.location.hash.slice(1);
+      const id = window.location.pathname.slice(1);
+      console.log(id);
+
+      if (!id) return;
 
     //* 1) Loading Recipe
     renderSpinner(recipeContainer)
         
     const res = await fetch(
       // 'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
-      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bca5d'
+      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
 
     );
     const data = await res.json();
@@ -165,3 +170,7 @@ const showRecipe = async function (){
   }
 }; 
 showRecipe();
+
+['hashchange', 'load'].forEach(ev => window.addEventListener(ev, showRecipe))
+// window.addEventListener('hashchange', showRecipe)
+// window.addEventListener('load', showRecipe)
