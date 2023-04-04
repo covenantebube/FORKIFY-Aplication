f1859d979ea971a0812bb7a4aabdb53f9d37820a
covenantebube <covenantebube1@gmail.com>
2023-04-04 02:08:05 -0700

    added timeout & getjson function in helpers

--- src/js/model.js
+++ src/js/model.js
@@ -1,19 +1,12 @@
 import { async } from "regenerator-runtime";
 import { API_URL } from "./config";
+import { getJSON } from "./helpers";
 export const state ={
  recipe: {}
 };
 export const loadRecipe = async function(id){
          try{
-    const res = await fetch(
-        `${API_URL}/${id}`
-  
-      );
-      const data = await res.json();
-   
-      if(!res.ok) throw new Error ( `${data.message} (${res.status})`)
-  
-       console.log(res, data);
+       const data = await getJSON(`${API_URL}/${id}`);
        const {recipe} = data.data;
        state.recipe = {
         id :recipe.id,
@@ -27,6 +20,7 @@ export const loadRecipe = async function(id){
        };
        console.log(state.recipe);
     }catch(err){
-        alert(err);
+        // Temp error handling
+       console.error(`${err}****`);
     }
 };
