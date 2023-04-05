2c44df0d007e6f191dcb05e328d697f2b97bb772
covenantebube <covenantebube1@gmail.com>
2023-04-05 06:50:47 -0700

    initial commit

--- src/js/config.js
+++ src/js/config.js
@@ -1,4 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '<YOUR_KEY>';
+export const KEY = 'd5fca0e4-b457-4a22-b304-b5596003a23d';
+export const MODAL_CLOSE_SEC = 2.5;
--- src/js/controller.js
+++ src/js/controller.js
@@ -1,10 +1,12 @@
 import { async } from 'regenerator-runtime';
 import * as model from './model.js'
+import { MODAL_CLOSE_SEC } from './config.js';
 import resultsView from './views/resultsView.js';
 import recipeView from './views/recipeView.js'
 import searchView from './views/searchView.js';
 import paginationView from './views/paginationView.js';
 import bookmarksView from './views/bookmarksView.js';
+import addRecipeView from './views/addRecipeView.js';
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime'
@@ -101,6 +103,36 @@ const controlBookmarks = function () {
   bookmarksView.render(model.state.bookmarks);
 };
 
+const controlAddRecipe = async function (newRecipe) {
+  try {
+    // Show loading spinner
+    addRecipeView.renderSpinner();
+
+    // Upload the new recipe data
+    await model.uploadRecipe(newRecipe);
+    console.log(model.state.recipe);
+
+    // Render recipe
+    recipeView.render(model.state.recipe);
+
+    // Success message
+    addRecipeView.renderMessage();
+
+    // Render bookmark view
+    bookmarksView.render(model.state.bookmarks);
+
+    // Change ID in URL
+    window.history.pushState(null, '', `#${model.state.recipe.id}`);
+
+    // Close form window
+    setTimeout(function () {
+      addRecipeView.toggleWindow();
+    }, MODAL_CLOSE_SEC * 1000);
+  } catch (err) {
+    console.error('💥', err);
+    addRecipeView.renderError(err.message);
+  }
+};
 
 
 
